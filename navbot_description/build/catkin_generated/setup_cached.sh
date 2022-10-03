1da06251d23d63d8efdab36fc36160bd854fab21
Jerin Peter <jerinpeter@gmail.com>
2022-10-03 12:29:16 +0530

    fixed frame base_link
    base_foot print set as parent - working

---
+++ navbot_description/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jerinpeter/testrobot_ws/src/navbot_description/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/jerinpeter/.local/bin:/home/jerinpeter/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/go/bin:/home/jerinpeter/go/bin:/usr/local/go/bin:/usr/local/go/bin'
+export PWD='/home/jerinpeter/testrobot_ws/src/navbot_description/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jerinpeter/testrobot_ws/src/navbot_description/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jerinpeter/testrobot_ws/src/navbot_description:$ROS_PACKAGE_PATH"
